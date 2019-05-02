433e63ce6d5d429ef4da30ab4c65eff07a00de11
Jaytrin <jaytrin.apinchapong@gmail.com>
2019-05-01 17:56:52 -0700

    added navbar and designed page with images

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './header.scss';
+import Nav from '../nav';
 
 export default props => {
     return(
-        <div>
-            <h1 className="center">This is a Header</h1>
+        <div className="header-container">
+            <div className="nav-header"></div>
+            <Nav />
+            <div className="header-img"></div>
+            <h2 className="header-title">We deliver cupcakes for the important events in your life!</h2>
+            <div className="header-bottom"></div>
         </div>
     )
 }
