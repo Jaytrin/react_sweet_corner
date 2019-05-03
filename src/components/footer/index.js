f10ae114f475e681c604e65a05a0941d0bcda535
Jaytrin <jaytrin.apinchapong@gmail.com>
2019-05-03 12:54:35 -0700

    created footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './footer.scss';
+import FooterDots from '../../assets/images/dots-footer.png'
+import Phone from '../../assets/images/phone.png';
 
 export default props => {
     return(
-        <div>
-            <h1 className="center">This is the Footer</h1>
+        <div className="footer">
+            <img className="footer-dots" src={FooterDots} alt="dots"/>
+            <p className="footer-copyright center">Copyright © 2014 Sweet Corner. All rights reserved.</p>
+            <img src={Phone} alt="phone-icon"/>
+            <p className="footer-phone">800 264-2099</p>
         </div>
     )
 }
