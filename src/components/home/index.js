d19c1ec567fed2dbaf11c1b7c4d815f7b1b4456a
Jaytrin <jaytrin.apinchapong@gmail.com>
2019-05-10 23:24:23 -0700

    adding sections with home content

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -4,7 +4,32 @@ import './home.scss';
 export default props => {
     return(
         <div>
-            <h1 className="center">Home Page</h1>
+            <section>
+                <h3>We cherish the sweet moments...</h3>
+                <p>
+                    Sweet Corner's story began first from our passion for sweets, which is inspired by our everyday job and also by the acknowledged weakness for sweets.
+                </p>
+                <p>
+                    For us, the Sweet corner means: the right amount of sweet, flawless appearance, the combination of shape, color and proportion and a right and balanced decor forthe event.
+                </p>
+                <p>
+                    We have been baking gourmet cupcakes, 100% from Scratch, since day one. We always use the finest and most natural ingredients we can find.
+                </p>
+            </section>
+
+            <section>
+                <h3>We can make happiness with so little!</h3>
+                <p>
+                    We can provide services for Weddings, Bar & Bat Mitzvahs, Birthdays, Showers, Corporate Events, and any other special occasion!
+                </p>
+                <p>
+                    Our expert bakers are waiting to create a memorable, unique cupcake bursting with freshness and flavor just for you.
+                </p>
+                <p>
+                Their delicious taste, great variety and their sweet universe, colorful and flavorful cupcakes make the best choice for a chic desert. How could you resist these delicious cupcakes?
+                </p>
+            </section>
+
         </div>
     )
 }
