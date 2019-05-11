fa6351e104762b0e5e7a1cb93fae80079c95fbac
Jaytrin <jaytrin.apinchapong@gmail.com>
2019-05-10 23:17:16 -0700

    updated spacing on footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -8,7 +8,7 @@ export default props => {
         <div className="footer">
             <img className="footer-dots" src={FooterDots} alt="dots"/>
             <p className="footer-copyright center">Copyright © 2014 Sweet Corner. All rights reserved.</p>
-            <img src={Phone} alt="phone-icon"/>
+            <img className="phone-icon" src={Phone} alt="phone-icon"/>
             <p className="footer-phone">800 264-2099</p>
         </div>
     )
